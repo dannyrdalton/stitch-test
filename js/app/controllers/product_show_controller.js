c96accf6b2a719ef26ae627dbd970430ece52b9d
Danny Dalton <drdalton@stanford.edu>
2013-10-14 10:52:24 -0700

    deleted product show controller

--- js/app/controllers/product_show_controller.js
+++
@@ -1,9 +0,0 @@
-app.controller('ProductShowCtrl', ['$scope', 'Shopify', '$state', function($scope, Shopify, $state) {
-	$scope.name = 'BLOOBY DOBY BOOBL';
-	$scope.product = Shopify.getCurrentProduct();
-	console.log($scope.product);
-	
-	$scope.$on('products', function(e, product) {
-		console.log('ewewewewew');
-	});
-}]);
